968148ac14a25cfe6df64faeb0e762e4d6d4407e
NurCeylin <ceylincetin03@hotmail.com>
2024-01-26 23:14:59 +0300

    al

--- GlobalGameJam/Assets/Scripts/PlayerMovement.cs
+++ GlobalGameJam/Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-
     private bool isMoving;
-
     private Vector2 input;
-
     public Animator animator;
     public Rigidbody2D rb;
-
-
 
     void Start()
     {
@@ -21,35 +16,33 @@ public class PlayerMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-
+        animator.SetBool("walk", true);
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
         rb.velocity = input * moveSpeed;
-        if (!isMoving)
+
+
+        if (input.x == 1)
         {
-
-
-            if (input.x == 1)
-            {
-                transform.localScale = new Vector3(1, 1, 1);
-            }
-
-            if (input.x == -1)
-            {
-                transform.localScale = new Vector3(-1, 1, 1);
-            }
-
-            if (input.x != 0) input.y = 0;
-
-            if (input != Vector2.zero)
-            {
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-
-            }
+            transform.localScale = new Vector3(1, 1, 1);
         }
+
+        if (input.x == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (input.x != 0) input.y = 0;
+
+        if (input != Vector2.zero)
+        {
+            var targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;
+
+
+        }
+
         else
         {
             animator.SetBool("walk", false);
@@ -57,22 +50,6 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    void Move(Vector3 targetPos)
-    {
-        animator.SetBool("walk", true);
-        isMoving = true;
 
-
-        // while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        // {
-
-        //     // transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-
-        // }
-        transform.position = targetPos;
-
-        isMoving = false;
-    }
-    // Update is called once per frame
 
 }
