968e1e3ad77e741ea77aaf4c7af1aa98df7b6522
İrem <s.iremozdemir@gmail.com>
2024-01-26 19:52:21 +0300

    Merge remote-tracking branch 'origin/Seden' into irem
    a

---
+++ GlobalGameJam/Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject Bubble;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CloseDialogue(){
+       // SetActive(false);
+    }
+
+    public void OpenDialogue(){
+        Debug.Log(Bubble);
+    }
+}
