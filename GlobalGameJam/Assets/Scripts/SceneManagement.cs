1121969b510641c0d8b2193623afe3d118d0c25e
NurCeylin <ceylincetin03@hotmail.com>
2024-01-28 02:44:18 +0300

    Revert "alınabilir?"
    This reverts commit 70870737d975be0d9a56c7176de8aeaf9896de36.

--- GlobalGameJam/Assets/Scripts/SceneManagement.cs
+++ GlobalGameJam/Assets/Scripts/SceneManagement.cs
@@ -10,39 +10,35 @@ public class SceneManagement : MonoBehaviour
     public static SceneManagement instance;
     public CanvasGroup VinImage;
 
-    private void Awake()
-    {
+    private void Awake() {
         instance = this;
     }
     int colorCount = 0;
 
+   
+
     public void StartGame()
-    {
-        SceneManager.LoadScene(1);
+    {   
+        SceneManager.LoadScene(1);    
     }
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
+    public void Update(){
+        if(Input.GetKeyDown(KeyCode.R)){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
 
-    public void IncreaseColorCount()
-    {
+    public void IncreaseColorCount(){
         colorCount += 1;
-        VinImage.LeanAlpha(0.33f * (3 - colorCount), 1f);
-        if (colorCount == 3)
-        {
-            Invoke("LoadEnding", 2.2f);
+        VinImage.LeanAlpha(0.33f * (3-colorCount), 1f);
+        if(colorCount == 3){
+           Invoke("LoadEnding", 2.2f);
         }
     }
 
-    void LoadEnding()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    void LoadEnding(){
+         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    
 }
